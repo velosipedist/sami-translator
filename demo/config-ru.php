5405634bb8fee0155c541f32c24931bbdf2dfd67
nikolay.konev <nikolay.konev@umisoft.ru>
2013-12-23 18:15:16 +0400

    Split configs by language

---
+++ demo/config-ru.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+use umi\sami\translator\TranslatorPlugin;
+
+$options = [
+    'build_dir' => getcwd() . '/docs/build/%version%',
+    'cache_dir' => getcwd() . '/docs/cache/%version%',
+    'default_opened_level' => 2,
+    'force' => true,
+];
+
+$iterator = Finder::create()
+    ->in(getcwd())
+    ->exclude('.git')
+    ->exclude('.idea')
+    ->exclude('vendor')
+    ->exclude('tests')
+    ->exclude('docs')
+    ->name('*.php');
+
+$sami = new Sami($iterator, $options);
+
+$lang = 'ru'; //todo pass from args
+
+$sami[TranslatorPlugin::ID] = new TranslatorPlugin($lang, $sami, [
+    'ignoreDocPatterns' => [
+        '/@inheritdoc/',
+        '/@copyright/'
+    ]
+]);
+
+return $sami;
