e62559ee8ff8547d2a25e082529ebbee7285918f
nikolay.konev <nikolay.konev@umisoft.ru>
2013-12-25 18:00:33 +0400

    fix keys extracting from reflections

--- src/velosipedist/sami/translator/ClassVisitor.php
+++ src/velosipedist/sami/translator/ClassVisitor.php
@@ -10,6 +10,7 @@ namespace velosipedist\sami\translator;
 
 use Sami\Parser\ClassVisitorInterface;
 use Sami\Reflection\ClassReflection;
+use Sami\Reflection\MethodReflection;
 use Sami\Reflection\ParameterReflection;
 use Sami\Reflection\PropertyReflection;
 use Sami\Sami;
@@ -45,6 +46,7 @@ class ClassVisitor implements ClassVisitorInterface
 
     /**
      * Build messages list indexed by unique signature
+     *
      * @param ClassReflection $class
      *
      * @return array [msgid=>[msgstr, Reflection]]
@@ -52,25 +54,86 @@ class ClassVisitor implements ClassVisitorInterface
     private function collectMessages(ClassReflection $class)
     {
         $messages = [
-            $class->getName() => [$class->getDocComment(), $class],
+            $this->classKey($class) => [$class->getDocComment(), $class],
         ];
         foreach ($class->getProperties() as $prop) {
-            /** @var $prop PropertyReflection */
-            $messages['var ' . $prop->getName()] = [$prop->getDocComment(), $prop];
+            $messages[$this->propertyKey($prop)] = [$prop->getDocComment(), $prop];
         }
         foreach ($class->getMethods() as $meth) {
-            $methodSignature = Arrays::from($meth->getParameters())
-                ->each(
-                    function (ParameterReflection $param) {
-                        return $param->getHintAsString() . ' $' . $param->getName();
-                    }
-                )
-                ->implode(', ')
-                ->obtain();
-            $methodKey = 'function ' . $meth->getName() . "($methodSignature)";
+            $methodSignature = $this->methodSignature($meth);
+            $methodKey = $this->methodKey($meth, $methodSignature);
             $messages[$methodKey] = [$meth->getDocComment(), $meth];
         }
 
         return $messages;
+    }
+
+    /**
+     * @param $prop
+     *
+     * @return string
+     */
+    private function propertyKey($prop)
+    {
+        return $this->accessString($prop) . $prop->getHintAsString() . ' $' . $prop->getName();
+    }
+
+    /**
+     * @param $meth
+     * @param $methodSignature
+     *
+     * @return string
+     */
+    private function methodKey($meth, $methodSignature)
+    {
+        return $this->accessString($meth) . 'function ' . $meth->getName() . "($methodSignature)";
+    }
+
+    /**
+     * @param PropertyReflection|MethodReflection $reflection
+     *
+     * @return string
+     */
+    private function accessString($reflection)
+    {
+        $ret = '';
+        $modifiers = (int) $reflection->toArray()['modifiers'];
+        $modifiersMap = [
+            ClassReflection::MODIFIER_ABSTRACT  => 'abstract',
+            ClassReflection::MODIFIER_FINAL     => 'final',
+            ClassReflection::MODIFIER_PRIVATE   => 'private',
+            ClassReflection::MODIFIER_PROTECTED => 'protected',
+            ClassReflection::MODIFIER_PUBLIC    => 'public',
+            ClassReflection::MODIFIER_STATIC    => 'static',
+        ];
+        foreach ($modifiersMap as $visibility => $modifierName) {
+            if (($modifiers & $visibility) > 0) {
+                $ret .= $modifierName . ' ';
+            }
+        }
+
+        return $ret;
+    }
+
+    private function methodSignature(MethodReflection $meth)
+    {
+        return Arrays::from($meth->getParameters())
+            ->each(
+                function (ParameterReflection $param) {
+                    return $param->getHintAsString() . ' $' . $param->getName();
+                }
+            )
+            ->implode(', ')
+            ->obtain();
+    }
+
+    /**
+     * @param ClassReflection $class
+     *
+     * @return string
+     */
+    private function classKey(ClassReflection $class)
+    {
+        return 'class ' . $class->getShortName();
     }
 }
